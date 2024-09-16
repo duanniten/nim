1845134b98b5da8929d6a595e49d5157d93925e8
duanniten <duann_@hotmail.com>
2024-09-16 12:07:02 -0300

    change update_q_value to convert state to tuple

--- nim.py
+++ nim.py
@@ -119,6 +119,7 @@ class NimAI():
         `alpha` is the learning rate, and `new value estimate`
         is the sum of the current reward and estimated future rewards.
         """
+        state = tuple(state)
         self.q[(state, action)] = (
             old_q +
             self.alpha * (
